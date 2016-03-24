c36ef960a1eb2ce7b47f977654fc88463864e732
sikB <sikhovski@icloud.com>
2016-03-24 10:44:18 -0400

    Clean up + options
    cleaned up the code; commented out what i did for hw. followed a
    different path. made options for the search (movies, tv, people etc);
    typeahead function preset to several people/movies

--- MovieApp.js
+++ MovieApp.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	var nowPlaying = baseURL + 'movie/now_playing' + apiKey;
 
 	$.getJSON(nowPlaying, function(movieData){
-		// console.log(movieData);
+		console.log(movieData);
 		var newHTML = '';
 		for(i=0; i<movieData.results.length; i++){
 			var currPoster = imagePath + 'w300' + movieData.results[i].poster_path;
@@ -24,23 +24,101 @@ $(document).ready(function(){
 		
 	});
 	
-	$('#movie-form').submit(function(){
-	var movieInput = $('#filter').val();
-	var searchMovie = 'http://api.themoviedb.org/3/search/movie' + apiKey + '&query=' + movieInput;
-	
-	$.getJSON(searchMovie, function(searchData){
+	$('#movie-form').submit(function(event){
+		// var userSearch = $('.typeahead').val();
+		var userSearch = $('#searchText').val();
+		var searchFilter = $('#searchFilter').val();
+		var searchURL = baseURL + 'search/' + searchFilter + apiKey + '&query=' + encodeURI(userSearch);
+		$.getJSON(searchURL, function(movieData){
 		var newHTML = '';
-		for(i=0; i<searchData.results.length; i++){
-			console.log(searchData.results[i]);
-			var currPoster = imagePath + 'w300' + searchData.results[i].poster_path;
-			newHTML += '<div class="col-sm-3">';
-			newHTML += '<img src="' + currPoster + '">';
-			newHTML += '</div>';
-		}
+			for(i=0; i<movieData.results.length; i++){
+				if((searchFilter == 'person')||((searchFilter == 'multi') && (movieData.results[i].media_type == 'person'))){
+					var currPoster = imagePath + 'w300' + movieData.results[i].profile_path;
+				}else{
+					var currPoster = imagePath + 'w300' + movieData.results[i].poster_path;
+				}
+				newHTML += '<div class="col-sm-3">';
+				newHTML += '<img src="' + currPoster + '">';
+				newHTML += '</div>';		
+			}
+		$('#poster-grid').html(newHTML);
 		
-			
 		});
-			event.preventDefault();
-		});
+		event.preventDefault();
+	});
 });
 
+var substringMatcher = function(strs){
+	return function findMatches (q, cb){
+		var matches, substringRegex;
+		matches= [];
+		substringRegex = new RegExp(q, 'i');
+		$.each(strs, function(i, str){
+			if(substringRegex.test(str)){
+				matches.push(str);
+			}
+		});
+		 cb(matches);
+	};
+};
+
+var actors = [
+	'Brad Pitt',
+	'Michael Douglas',
+	'Al Pacino'
+	];
+
+$('#movie-form .typeahead').typeahead({
+	hint: true,
+	highlight: true,
+	minLength: 1
+},
+{
+	name: 'actors',
+	source: substringMatcher(actors)
+});
+
+
+
+
+
+
+
+
+// var movieInput = $('#filter').val();
+// 	var res = encodeURI(movieInput);
+// 	var searchKeyWord = 'http://api.themoviedb.org/3/search/multi' + apiKey + '&query=' + res;
+// 	console.log(searchKeyWord);
+// 	$.getJSON(searchKeyWord, function(searchData){
+// 		var newHTML = '';
+// 		for(i=0; i<searchData.results.length; i++){
+// 			// console.log(searchData.results[i]);
+// 			var currPoster = imagePath + 'w300' + searchData.results[i].poster_path;
+// 			newHTML += '<div class="col-sm-3">';
+// 			newHTML += '<img src="' + currPoster + '">';
+// 			newHTML += '</div>';
+// 			console.log(currPoster);
+// 		}
+// 		$('#poster-grid').html(newHTML);
+			
+// 		});
+// 			event.preventDefault();
+// 		});
+// 	// Search By person
+
+// 	$('#movie').select(function(){
+// 		var movieInput = $('#filter').val();
+// 		var res = encodeURI(movieInput);
+// 		var searchMovie = 'http://api.themoviedb.org/3/search/movie' + apiKey + '&query=' + res;
+// 		$.getJSON(searchKeyWord, function(movieData){
+// 		var newHTML = '';
+// 		for(i=0; i<movieData.results.length; i++){
+// 			var currPoster = imagePath + 'w300' + movieData.results[i].poster_path;
+// 			newHTML += '<div class="col-sm-3">';
+// 			newHTML += '<img src="' + currPoster + '">';
+// 			newHTML += '</div>';
+// 			}
+// 			$('#poster-grid').html(newHTML);
+// 		});
+// 		event.preventDefault();
+// 	});
